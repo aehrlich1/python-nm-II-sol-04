a83db8037b2c97051fbf7a257f5358142934dab2
aehrlich1 <anatol.ehrlich@gmail.com>
2022-01-09 13:15:24 +0100

    added question 5: svd decomposition

--- main.py
+++ main.py
@@ -33,10 +33,84 @@ z = 1/np.sqrt(3)*np.array([1, 1, 1], dtype=np.float64)
 def page_rank(A, p):
     n = len(A)
     v = np.ones(n, dtype=np.float64) / n  # rank vector
-    M = (1 - p) * A + p * np.ones((n,n)) / n
+    M = (1 - p) * A + p * np.ones((n, n)) / n
     return pow_iteration(M, v)
 
 
 A = np.array([[0, 1, 0], [0.5, 0, 1], [0.5, 0, 0]], dtype=np.float64)
 p = 0.15
-print(page_rank(A, p))
+#print(page_rank(A, p))
+
+
+# Question 04
+def qr_algorithm(A):
+    """
+    Compute thr qr algorithm of the given matrix
+
+    Parameters
+    ----------
+    A : matrix
+        A matrix array
+
+    Returns
+    -------
+    q : 
+
+
+    """
+    e = 1e-5
+    i = 0
+
+    while True:
+        q, r = np.linalg.qr(A)
+        A_k = r @ q
+
+        delta_norm = np.abs(np.linalg.norm(A) - np.linalg.norm(A_k))
+
+        if(delta_norm < e):
+            return A_k, q
+        else:
+            A = A_k
+            i += 1
+
+        if(i > 1e4):
+            print("No convergence")
+            break
+
+
+def sym(n):
+    """
+    Generate a symmetric matrix of dimension n x n
+
+    Parameters
+    ----------
+    n : int
+        A matrix array
+
+    """
+    A = np.random.rand(n, n)
+    A = (A + A.T) / 2
+
+    return A
+
+
+A, Q = qr_algorithm(sym(5))
+#print(A)
+
+
+# Question 05
+A = np.random.rand(15, 30)
+u, s, vh = np.linalg.svd(A)
+
+sigma = np.zeros((15, 30))
+euclidean = np.sqrt(np.sum(s[10:-1]))
+s[10:-1] = 0                                # set the last 5 singular values to 0
+np.fill_diagonal(sigma, s)                  # diagonal matrix with `s` on the main diagonal
+
+A_10 = u @ sigma @ vh
+frobenius = np.linalg.norm(A - A_10)
+
+
+print("Question 5\n-------------------------------------------")
+print(f'{"Frobenius":20} {"==>":15} {frobenius:.3f}')
+print(f'{"Euclidean":20} {"==>":15} {euclidean:.3f}')
